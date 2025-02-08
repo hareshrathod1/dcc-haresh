4c0b8f1dd7da1918ad42c812201401a3ba5841e7
hareshrathod1 <24110128@iitgn.ac.in>
2025-02-08 14:49:17 +0530

    Add files via upload

--- main.c
+++ main.c
@@ -6,33 +6,50 @@ Write your code in this editor and press "Run" button to compile and execute it.
 
 *******************************************************************************/
 
-#include <stdio.h>
+ #include <stdio.h>
+ #include <assert.h>
 
+ void minmax(int xs[], int n, int *min, int *max)
+ {
+     assert(min && max);
 
-    
+     if (n <= 0) return;
 
-struct point {
-    int x;
-    int y;
-};
+     /* initialize */ *min = *max = xs[0];
 
-void drop_to_x_axis(struct point *p) {
-    if (p != NULL) { 
-        p->y = 0;
-    }
-}
+     size_t i;
+     for (i = 1; i < n-1; i += 2) {
+	  int m, M;
 
-int main() {
-    struct point my_point = {5, 10};
-    printf("Original point: x = %d, y = %d\n", my_point.x, my_point.y);
+	  if (xs[i] < xs[i+1]) {
+	      m = xs[i];
+	      M = xs[i+1];
+	  } else {
+	      m = xs[i+1];
+	      M = xs[i];
+	  }
 
-    drop_to_x_axis(&my_point); 
+	  if (m < *min) *min = m;
+	  if (M > *max) *max = M;
+     }
 
-    printf("Point after dropping to x-axis: x = %d, y = %d\n", my_point.x, my_point.y); 
-    struct point *null_point = NULL;
-    drop_to_x_axis(null_point); 
+     if (i < n) {
+	if (xs[i] < *min) *min = xs[i];
+	if (xs[i] > *max) *max = xs[i];
+     }
+ }
 
-    return 0;
-}
+ int main()
+ {
+     int xs[] = { 1,2,-4,9,-8,7,5,0,-9,8,6,665,5,5,5,57,-9,-9,87,-0,-7 };
+     int m, M;
 
-    
+     minmax(xs, 9, &m, &M);
+     printf("%d %d\n", m, M);
+
+     minmax(xs, 7, &m, &M);
+     printf("%d %d\n", m, M);
+     
+     return 0;
+
+ }
